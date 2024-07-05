892bf2f716cf10f36eea9774ecb5c7e507c9c0d8
Eric Penrod <emp923@gmail.com>
2024-07-05 14:53:42 -0400

    basic navigation

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,16 @@
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+        molestiae ipsam, et aut consequatur ipsum voluptates quasi, quos
+        recusandae doloribus provident consequuntur amet nobis est voluptate
+        perferendis quaerat distinctio saepe dolores perspiciatis ex ab nostrum
+        eaque! Porro perspiciatis possimus, sed a quidem sunt sit doloremque
+        molestiae maiores blanditiis quasi quod.
+      </p>
+    </div>
   );
 };
 
