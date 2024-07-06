f5f4d8d6b5d78d9fb9a27b53659416522e7f4157
Eric Penrod <emp923@gmail.com>
2024-07-06 08:56:32 -0400

    search courses api

--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 async function fetchCourses() {
-  const response = await fetch('https://localhost:3000/api/courses');
+  const response = await fetch('http://localhost:3000/api/courses');
   const courses = await response.json();
   return courses;
 }
